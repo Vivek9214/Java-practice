dd6aa255123434a0d2ac95ff5c5d70ff8d60e94e
CoderVivek92 <vivekgahlot92@gmail.com>
2024-12-26 21:59:08 +0530

    Add files via upload

---
+++ Program2/Five.java
@@ -0,0 +1,11 @@
+package ProjectClg.Day2;
+public class Five {
+    public static void main(String[] args) {
+        int num = 5;
+        int result =1;
+        for(int i = 1; i<= num; i++){
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
