d46c93c189689ca15c63097f1bb46cc618238e08
CoderVivek92 <vivekgahlot92@gmail.com>
2025-01-07 21:50:59 +0530

    Add files via upload

---
+++ Execises/Loop/Loop2.java
@@ -0,0 +1,21 @@
+public class Loop2 {
+    public static void main(String[] args) {
+        // 2. Pattern
+        int m = 5;
+        int n = 4;
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= m; j++) {
+                // cell -> (i,j)
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
