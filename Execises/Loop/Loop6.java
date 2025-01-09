c834528b3d79e4a7507d94e64434c5ac25fe746f
CoderVivek92 <vivekgahlot92@gmail.com>
2025-01-09 20:13:25 +0530

    Add files via upload

---
+++ Execises/Loop/Loop6.java
@@ -0,0 +1,14 @@
+public class Loop6 {
+    public static void main(String[] args) {
+        //     6.Pattern
+        int n = 1; 
+        for(int i= n; i<=6; i++){
+            int m= 1;
+            for(int j =1; j<=i; j++){
+                System.out.print(m);
+                m++;
+            }
+            System.out.println();
+        }
+    }
+}
