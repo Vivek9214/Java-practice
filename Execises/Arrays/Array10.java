e5685040cf1604b9913c59f51433e48c5e6f408f
CoderVivek92 <vivekgahlot92@gmail.com>
2025-01-21 21:55:59 +0530

    Update Array10.java

--- Execises/Arrays/Array10.java
+++ Execises/Arrays/Array10.java
@@ -11,10 +11,8 @@ public class Array10 {
         
         for(int i = 0; i< n; i +=k){
             int left = i; 
-            //to handle case when k is not multiple of n
             int right = Math.min(i + k -1, n - 1);
             int temp;
-            //reverse the sub-array [left, right]
             while(left < right){
                 temp = arr[left];
                 arr[left] = arr[right];
