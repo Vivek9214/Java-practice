d18351f89e4caf725886a87ccc008084e0edad8e
CoderVivek92 <vivekgahlot92@gmail.com>
2025-01-29 21:41:58 +0530

    Add files via upload

---
+++ Execises/Recursion/Recursion7.java
@@ -0,0 +1,20 @@
+package Project.Recurtion;
+// print x^n (stack height= n)
+public class Recurtion7 {
+    public static int printpow(int x,int n){
+        if(n==0){     //base case 1
+            return 1;
+        }             
+        if(x == 0){   //base case 2
+            return 0;
+        }
+        int xPownm1 = printpow(x, n-1);
+        int xPown = x * xPownm1;
+        return xPown;
+    }
+    public static void main(String[] args){
+        int x = 2, n = 5;
+        int ans = printpow(x,n);
+        System.out.println(ans);
+    }
+}
