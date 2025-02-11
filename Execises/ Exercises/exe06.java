b33281547fb732f1efeb615253a41c1934bc6504
CoderVivek92 <vivekgahlot92@gmail.com>
2025-02-11 14:52:44 +0530

    Add files via upload

---
+++ Execises/ Exercises/exe06.java
@@ -0,0 +1,11 @@
+public class exe6 {
+    public static void main(String[] args){
+        int i;
+        int j=1;
+        do{
+            j++;
+            i=j;
+        }while(i<1000000);
+                     System.out.print(i);
+    }
+}
