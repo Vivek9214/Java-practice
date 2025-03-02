d09e8b3750badfed4a03ae40b1cc46e6e7580014
CoderVivek92 <vivekgahlot92@gmail.com>
2025-03-02 12:04:56 +0530

    Add files via upload

---
+++ Execises/Loop/Loop1.java
@@ -0,0 +1,13 @@
+public class Loop1 {
+    public static void main(String[] args) {
+    // 1.Pattern 4*4
+    for(int i= 1; i<=4; i++){
+    for(int j = 1; j<=5; j++){
+        System.out.print("*");
+    }
+        System.out.println(" ");
+    }
+    }
+}
+
+
