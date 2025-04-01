8ae658ce21aabde253cb8f24aefc70f82ced76c8
CoderVivek <vivekgahlot92@gmail.com>
2025-04-01 14:35:55 +0530

    Update DataTypeFitting.java

--- Program2/DataTypeFitting.java
+++ Program2/DataTypeFitting.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class DataTypeFitting {
+public class Solution {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = 1;
-        for(int i = 0; i < T; i++){
-            try{
-            String numstr = sc.next();
-            Long num = Long.parseLong(numstr);
-            System.out.println(num + " can be fitted in :");
-            if(num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE){
-                System.out.println("* Byte ");
-            }
-            if(num >= Short.MIN_VALUE && num <= Short.MAX_VALUE){
-                System.out.println("* Short ");
-            }
-            if(num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE){
-                System.out.println("* Integer ");
-            }
-            if(num >= Long.MIN_VALUE && num <= Long.MAX_VALUE){
-                System.out.println("* Long ");
-            }
-        }catch ( Exception e ){
-            System.out.println(" can't be fitted anywhere.");
+        int n = sc.nextInt();
+        for(int i =0; i<n; i++){
+        String numstr = sc.next();
+        try{
+        long num = Long.parseLong(numstr);
+        System.out.println(num + " can be fitted in:");
+        if(num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE){
+            System.out.println("* byte");
         }
-    
+        if(num >= Short.MIN_VALUE && num <= Short.MAX_VALUE){
+            System.out.println("* short");
         }
+        if(num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE){
+            System.out.println("* int");
+        }
+        if(num >= Long.MIN_VALUE && num <= Long.MAX_VALUE){
+            System.out.println("* long");
+        }
+        }
+        catch(Exception e){
+            System.out.println(numstr + " can't be fitted anywhere.");
+        }}
     }
-}
+} 
