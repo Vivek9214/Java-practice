9f06b8f1e2d103815ca4e3808a4f00548bd27093
CoderVivek <vivekgahlot92@gmail.com>
2025-05-26 14:00:49 +0530

    Delete Execises/Compare.java

--- Execises/Compare.java
+++
@@ -1,9 +0,0 @@
-import java.util.regex.*;
-public class Compare {
-    public static void main(String[] args){
-        Pattern patterns = Pattern.compile("a");
-        Matcher matcher = patterns.matcher("a");
-        boolean matches = matcher.matches();
-        System.out.println(matches);
-    }
-}
